80b1fc425fa44be3665ef5ad2876c433335df96e
Derek Carter <derek.carter@endurance.com>
2016-09-13 13:50:22 -0600

    provisioner working

--- provision/shell.sh
+++ provision/shell.sh
@@ -4,9 +4,12 @@ yum -y install httpd mod_ssl perl-Test-Harness perl-Test-Simple perl-CGI-Applica
 
 chkconfig httpd on
 service httpd start
-ln -s /vagrant/provision/secret.conf /etc/httpd/conf.d/
-#htpasswd -c /etc/httpd/registry.users goozbach
-ln -s /vagrant/provision/mgmt/ /var/www/
-ln -s /vagrant/provision/registry.groups /etc/httpd/
-ln -s /vagrant/provision/registry.users /etc/httpd/
-ln -s /vagrant/lib/Web/ /usr/local/share/perl5/
+ln -fs /vagrant/provision/secret.conf /etc/httpd/conf.d/
+ln -fs /vagrant/provision/mgmt/ /var/www/
+ln -fs /vagrant/provision/registry.groups /etc/httpd/
+mkdir -p /usr/local/share/perl5
+ln -fs /vagrant/lib/Web/ /usr/local/share/perl5/
+touch /etc/httpd/registry.users
+echo -n 'cangetin' | htpasswd -ic /etc/httpd/registry.users goozbach
+httpd -S && service httpd restart
+
